5404fc85cea1bf77f19004854ab507d0d0d9de51
jahnava <jahnavalmt@gmail.com>
2017-03-10 20:00:30 -0500

    cloned file due to minor issues

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
+
+//powerup -- middleware
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 //connect to the database
 mongoose.connect(mongoURI);
@@ -31,6 +36,7 @@ var Animal =mongoose.model('Animal', animalSchema);
 //     console.log(data);
 //   }
 // });
+
 //GET /animals
 server.get('/animals', function(req, res){
   Animal.find({}, function(err, documents){
@@ -59,9 +65,52 @@ server.get('/animals/:id', function(req, res){
     }
   });
 });
-//POST /animals
+//POST /animals //added info into postman and clicked send //appeared on git with id
+server.post('/animals', function(req, res){
+  var animal = new Animal(req.body);
+// console.log(animal);
+//   res.send('it works'); //used these to check git to make sure its logged
+animal.save(function(err, document){
+  if(err){
+    res.status(500).json({
+      msg:err
+    });
+  }else{
+      res.status(200).json({
+  msg: 'Success'
+});
+}
+});
+});
 //PUT /animals/:id
+server.put('/animals/:id', function(req, res){
+  Animal.findOneAndUpdate({ _id: req.params.id}, req.body, function(err, document){
+    if(err){
+      res.status(500).json({ //500 is the number name to run something
+        msg: err
+      });
+      }else{
+          res.status(200).json({
+      msg: 'Successfully updated '
+      });
+    }
+  });
+});
+
 //DELETE /animals/:id
+server.delete('/animals/:id',function(req, res){
+  Animal.remove({_id: req.params.id}, function(err, document){ //use {_id: req.params.id, function(err, document) to get id
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully delete'
+      });
+    }
+  });
+});
 server.listen(port, function(){
   console.log('Now listening on port...', port);
 });
